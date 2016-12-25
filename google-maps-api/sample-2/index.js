cbf60b1c0e9c4622a32c998fb4c15d84ae0d9a8b
namvh <vhnam2504@gmail.com>
2016-12-25 19:44:59 +0700

    fix sample-2

--- google-maps-api/sample-2/index.js
+++ google-maps-api/sample-2/index.js
@@ -5,6 +5,10 @@ var cyrusMap = $('#cyrus-map');
 function initMap() {
     map = new google.maps.Map(cyrusMap.get(0), {
         zoom: 14,
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
+            position: google.maps.ControlPosition.TOP_RIGHT
+        },
         center: {
             lat: 10.7607409,
             lng: 106.6829062
