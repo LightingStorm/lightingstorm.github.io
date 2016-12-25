60e49eaa613d864f524a46b7e912099e0d5c420d
namvh <vhnam2504@gmail.com>
2016-12-25 19:41:18 +0700

    fix sample-2

--- google-maps-api/sample-2/index.js
+++ google-maps-api/sample-2/index.js
@@ -2,19 +2,20 @@ var map;
 var marker;
 var cyrusMap = $('#cyrus-map');
 
-var location = {
-    lat: 10.7607409,
-    lng: 106.6829062
-};
-
 function initMap() {
     map = new google.maps.Map(cyrusMap.get(0), {
         zoom: 14,
-        center: location
+        center: {
+            lat: 10.7607409,
+            lng: 106.6829062
+        }
     });
 
     marker = new google.maps.Marker({
-        position: location,
+        position: {
+            lat: 10.7607409,
+            lng: 106.6829062
+        },
         map: map
     });
 }
