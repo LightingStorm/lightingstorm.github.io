4f171671fff5309098cfc13a4c1429cdd1448bc9
namvh <vhnam2504@gmail.com>
2016-12-25 19:34:12 +0700

    update sample-2

---
+++ google-maps-api/sample-2/index.js
@@ -0,0 +1,20 @@
+var map;
+var marker;
+var cyrusMap = $('#cyrus-map');
+
+var location = {
+    lat: 10.7607409,
+    lng: 106.6829062
+};
+
+function initMap() {
+    map = new google.maps.Map(cyrusMap, {
+        zoom: 14,
+        center: location
+    });
+
+    marker = new google.maps.Marker({
+        position: location,
+        map: map
+    });
+}
