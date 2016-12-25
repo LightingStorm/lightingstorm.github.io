93c8726a56984ee47f861c228c87de0ae8cbfed9
namvh <vhnam2504@gmail.com>
2016-12-25 19:23:58 +0700

    update key

--- google-maps-api/sample-1/index.js
+++ google-maps-api/sample-1/index.js
@@ -14,7 +14,7 @@ if( ! document.getElementById('googleMapAPI') ){
 	var s = document.createElement('script');
 	s.type = 'text/javascript';
 	s.id = 'googleMapAPI';
-	s.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places&key=AIzaSyAomm2lk8EF5tk3lToOcRspCdtqzEindf0&callback=controlMap';
+	s.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places&key=AIzaSyBkds9ggyRFpcqMms_ilugLUZaflz-UQSU&callback=controlMap';
 	document.body.appendChild(s);
 }else{
 	controlMap();
